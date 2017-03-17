30bacae2531fda2c703d00ebb2ef2b3d8ba7639a
Artur Paikin <artur@arturpaikin.com>
2017-03-17 12:03:16 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var insertCss = require('insert-css')
+
+module.exports = css
+
+function css (template) {
+  var args = arguments
+
+  var expressions = Object.keys(args).map(function (i) {
+    return args[i]
+  })
+  expressions.shift()
+
+  var style = template.reduce(function (accumulator, part) {
+    return accumulator + expressions.shift() + part
+  })
+
+  if (global.document) {
+    insertCss(style)
+  }
+
+  return style
+}
